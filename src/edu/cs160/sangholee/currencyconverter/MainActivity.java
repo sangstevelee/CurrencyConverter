ac170f1a48778402d100aee5b0735b5feb271677
Steve Lee <stevelee2030@gmail.com>
2014-01-30 22:50:10 -0800

    Second commit, layout finished.

--- src/edu/cs160/sangholee/currencyconverter/MainActivity.java
+++ src/edu/cs160/sangholee/currencyconverter/MainActivity.java
@@ -1,8 +1,10 @@
 package edu.cs160.sangholee.currencyconverter;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +12,9 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		setupSpinners();
+		
+
 	}
 
 	@Override
@@ -17,6 +22,28 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+	
+	public void setupSpinners() {
+		Spinner spinner = (Spinner) findViewById(R.id.fromSpinner);
+		// Create an ArrayAdapter using the string array and a default spinner layout
+		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+		        R.array.currency_array, android.R.layout.simple_spinner_item);
+		// Specify the layout to use when the list of choices appears
+		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		// Apply the adapter to the spinner
+		spinner.setAdapter(adapter);
+		
+		Spinner spinner1 = (Spinner) findViewById(R.id.toSpinner);
+		// Create an ArrayAdapter using the string array and a default spinner layout
+		ArrayAdapter<CharSequence> adapter1 = ArrayAdapter.createFromResource(this,
+		        R.array.currency_array, android.R.layout.simple_spinner_item);
+		// Specify the layout to use when the list of choices appears
+		adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		// Apply the adapter to the spinner
+		spinner1.setAdapter(adapter1);
+		
+		
 	}
 
 }
